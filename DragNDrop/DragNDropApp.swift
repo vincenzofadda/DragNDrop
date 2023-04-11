d8bff1b16f2280ba2a94b1bb907e4fc54e72d6d1
Vincenzo Silva Fadda <vincenzofadda@gmail.com>
2023-04-11 14:59:19 -0300

    Initial Commit

---
+++ DragNDrop/DragNDropApp.swift
@@ -0,0 +1,17 @@
+//
+//  DragNDropApp.swift
+//  DragNDrop
+//
+//  Created by user on 11/04/23.
+//
+
+import SwiftUI
+
+@main
+struct DragNDropApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
