73ce48263d6960a6eda5118b3af3047596c5a1fa
Vincenzo Silva Fadda <vincenzofadda@gmail.com>
2023-04-11 16:19:01 -0300

    making a drag and drop code

--- DragNDrop/ContentView.swift
+++ DragNDrop/ContentView.swift
@@ -8,14 +8,31 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var isDragging = false
+    @State var position = CGPoint.zero
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+        RoundedRectangle(cornerRadius: 20, style: .continuous)
+            .frame(width: 100, height: 100)
+            .position(x: position.x, y: position.y)
+            .foregroundColor(isDragging ? Color.green : Color.blue)
+            .gesture(
+                DragGesture()
+                    .onChanged({ value in
+                        position = value.location
+                        isDragging = true
+                    })
+                    .onEnded { _ in
+                        isDragging = false
+                    }
+            )
+            .onAppear {
+                // Geometry Reader
+                position = CGPoint(
+                    x: (UIScreen.main.bounds.width/2),// - 100,
+                    y: (UIScreen.main.bounds.height/2) - 100
+                )
+            }
     }
 }
 
